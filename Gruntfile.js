3388914bc7970112b43cbeb784020d6f0a8a562f
mcekiera <marcin.cekiera@gmail.com>
2016-10-26 15:09:45 +0200

    Basic structure set up

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) { 
+	// Project configuration. 
+	grunt.initConfig({ 
+		sass: { 
+			options: { 
+				sourceMap: true 
+			}, 
+				dist: { 
+				files: { 
+					'css/style.css': 'scss/style.scss' 
+				} 
+			} 
+		},
+		watch: { 
+			scripts: { 
+				files: ['scss/*.scss'], 
+				tasks: ['sass'], 
+				options: { 
+					spawn: false, 
+				}, 
+			} 
+		},
+		browserSync: {
+			default_options: {
+				bsFiles: {
+					src: [
+						"*js",
+						"js/*.js",
+						"css/*.css",
+						"scss/*.scss",
+						"*.html"
+					]
+				},
+					options: {
+						watchTask: true,
+					server: {
+						baseDir: "./"
+					}
+				}
+			}
+		}
+
+	}); 
+	// Load the plugins tasks 
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	// Default task(s). 
+
+	grunt.registerTask('default', ['browserSync', 'watch']); 
+};
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+/* Layout elements */
+
+var newGameElem = document.getElementById('js-newGameElement'), 
+		pickElem = document.getElementById('js-playerPickElement'), 
+		resultsElem = document.getElementById('js-resultsTableElement');
+
+/* Control elements */
+var newGame = document.getElementById('js-newGameButton'),
+		playerPointsElem = document.getElementById('js-playerPoints'), 
+		playerNameElem = document.getElementById('js-playerName'), 
+		computerPointsElem = document.getElementById('js-computerPoints');
